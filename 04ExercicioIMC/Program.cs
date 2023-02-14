f25739dc54a6a390b213b6d05f5f5a3700d6d014
MarianeCamilo <marianecamilo03@gmail.com>
2023-02-14 17:59:08 -0300

    ExercicioIMC

---
+++ 04ExercicioIMC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04ExercicioIMC
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            // instanciar o objeto da classe pessoa
+            Pessoa p = new Pessoa();
+            p.altura = 1.70;
+            p.peso = 53;
+            p.mensagem();
+        }
+    }
+}
