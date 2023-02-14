935b02d0470ea7ebb01cf781ee8d43dbf66ed359
MarianeCamilo <marianecamilo03@gmail.com>
2023-02-14 18:11:30 -0300

    Acessos

---
+++ 05Acesso/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _05Acesso
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            
+        }
+    }
+}
