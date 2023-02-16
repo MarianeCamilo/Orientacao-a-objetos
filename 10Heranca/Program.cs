58ba368c3b0c07a91fb45a3b444775523e75b31a
MarianeCamilo <marianecamilo03@gmail.com>
2023-02-16 17:09:29 -0300

    Herança

---
+++ 10Heranca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colaborador obj = new Colaborador("Mari", 20, 2000);
+        }
+    }
+}
